58de993b2500f598193a460d21e1bb1cd8e997c7
Modosip <37976013+Modosip@users.noreply.github.com>
2018-11-11 23:33:22 +0300

    Add files via upload

--- PromoPrice.php
+++ PromoPrice.php
@@ -3,12 +3,16 @@ if (file_exists("promoPrice.csv")) echo "Файл существует: promoPri
 if (file_exists("wheelArticles.csv")) echo "Файл существует: wheelArticles.csv <br>";
 $line = file("promoPrice.csv", FILE_IGNORE_NEW_LINES);
 $norArticle = file("wheelArticles.csv", FILE_IGNORE_NEW_LINES);
+echo count($norArticle);
 foreach ($line as $line_num => $line) {
-    echo "Строка #<b>{$line_num}</b> : " . $line;
-    $temp = explode(';', $line);
-   // print_r($temp);
-    echo "<b> - " . $temp[12] = $temp[12] + 1 . "</b><br />\n";
+    foreach ($norArticle as $norArticle_num => $norArticle) {
+    $tempOne = explode(';', $line);
+    $tempTwo = explode(';', $norArticle_num);
+        for ($i = 0; $i < count($norArticle); $i++) {
+            if $tempOne [] == $tempTwo
+
+        }
+        echo "<b> - " . $temp[12] = $temp[12] + 1 . "</b><br />\n";
+    }
 }
 ?>
-
-
