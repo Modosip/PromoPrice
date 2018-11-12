670a2540334eb78047b81123004391276b24be1a
Modosip <37976013+Modosip@users.noreply.github.com>
2018-11-13 00:23:40 +0300

    Add files via upload

--- PromoPrice.php
+++ PromoPrice.php
@@ -3,16 +3,21 @@ if (file_exists("promoPrice.csv")) echo "Файл существует: promoPri
 if (file_exists("wheelArticles.csv")) echo "Файл существует: wheelArticles.csv <br>";
 $line = file("promoPrice.csv", FILE_IGNORE_NEW_LINES);
 $norArticle = file("wheelArticles.csv", FILE_IGNORE_NEW_LINES);
-echo count($norArticle);
+$fd = fopen("New.txt", 'w') or die("не удалось создать файл");
 foreach ($line as $line_num => $line) {
-    foreach ($norArticle as $norArticle_num => $norArticle) {
     $tempOne = explode(';', $line);
-    $tempTwo = explode(';', $norArticle_num);
-        for ($i = 0; $i < count($norArticle); $i++) {
-            if $tempOne [] == $tempTwo
-
-        }
-        echo "<b> - " . $temp[12] = $temp[12] + 1 . "</b><br />\n";
-    }
+    for ($i = 0; $i < count($tempOne)-2; $i++) {
+            foreach ($norArticle as $norArticle_num => $norArticle) {
+                echo $norArticle . "/r" ;
+                $tempTwo = explode(';', $norArticle);
+                       if ($tempOne [$i] == $tempTwo [1]) {
+                           $tempTwo [2] = $tempOne [count($tempOne)-1];
+                           fwrite($fd, $tempTwo [0] . ";");
+                           fwrite($fd, $tempTwo [1] . ";");
+                           fwrite($fd, $tempTwo [2] . ";/r ");
+                        }
+            }
+    }  
+fclose($fd);             
 }
 ?>
